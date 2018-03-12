1c888631a5de46f77a540940b2419091ed0fd82f
MaxwellM <maxwellrmorin@gmail.com>
2018-03-12 17:37:24 -0600

    Second Commit!

--- main.go
+++ main.go
@@ -1,2 +1,16 @@
 package PortfolioWebsite
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/css", "./web/css")
+	router.Static("/js", "./web/js")
+	router.Static("/html", "./web/html")
+	router.StaticFile("/", "./web/html/index.html")
+
+	router.Run(":8080")
+}
