c6cc5e6d177f0d17a16032b874bbf1dbee17339c
MaxwellM <maxwellrmorin@gmail.com>
2018-03-13 19:33:51 -0600

    Fifth commit!

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 func main() {
 	router := gin.Default()
 
+	//sets the root of the directory to access
 	router.Static("/css", "./web/css")
 	router.Static("/js", "./web/js")
+	router.Static("/images", "./web/images")
 	router.Static("/html", "./web/html")
 	router.StaticFile("/", "./web/html/index.html")
 
