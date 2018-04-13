dca85e2d8be260321697da7f70f97d9b45171179
MaxwellM <maxwellrmorin@gmail.com>
2018-04-12 18:13:37 -0600

    Updated Contact Me and Firefox formatting.

--- web/js/app.js
+++ web/js/app.js
@@ -2,4 +2,15 @@ var ngModule = angular.module('app', []);
 
 ngModule.controller('myCtrl', ['$scope', '$http', '$q', '$filter', function ($scope, $http, $q, $filter) {
 
+    function myMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(40.586667, -111.861244),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }
+
+    myMap();
+
 }]);
