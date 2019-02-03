ab39395cb732585f42d8c3afd582b55e7c28c9f7
MaxwellM <maxwellrmorin@gmail.com>
2019-02-02 21:47:27 -0700

    Server has its own sh run file.

---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+export PGUSER=postgres
+export PGPASSWORD=Maxwellmorin1
+export PGPORT=5432
+export PGDATABASE=portfoliowebsite_db
+
+cd web
+./buildwebsite.py
+cd ..
+
+go install
+/root/bin/PortfolioWebsite
