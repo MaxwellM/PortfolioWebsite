7bb8712467687e74d09a5a79c9c61788363b1e80
MaxwellM <maxwellrmorin@gmail.com>
2019-06-27 17:13:19 -0600

    SSO and Contact update.

--- goResources/contactMe/contactMe.go
+++ goResources/contactMe/contactMe.go
@@ -2,16 +2,24 @@ package contactMe
 
 import (
 	"gopkg.in/mail.v2"
+	"strings"
 )
 
 
 func SendEmail(name, email, phone, message string) error {
+	messageBody := []string{}
+	messageBody = append(messageBody, "Name: "+name)
+	messageBody = append(messageBody, "Email: "+email)
+	messageBody = append(messageBody, "Phone: "+phone)
+	messageBody = append(messageBody, "Message: "+message)
+	messageBodyStr := strings.Join(messageBody, "\n")
+
 	m := mail.NewMessage()
 	m.SetHeader("From", "maxintosh.mailer@gmail.com")
 	m.SetHeader("To", "maxwellrmorin@gmail.com")
 	m.SetAddressHeader("Cc", email, "Mailer")
 	m.SetHeader("Subject", "Maxintosh Contact Request")
-	m.SetBody("text/html", message)
+	m.SetBody("text/plain", messageBodyStr)
 
 	d := mail.NewDialer("smtp.gmail.com", 587, "maxintosh.mailer", "17DesertHawk!")
 	d.StartTLSPolicy = mail.MandatoryStartTLS
