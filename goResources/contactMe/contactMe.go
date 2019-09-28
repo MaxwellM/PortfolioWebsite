f6466725f4bc47819e7ec78108f0266797f5bc05
MaxwellM <maxwellrmorin@gmail.com>
2019-09-28 09:10:50 -0600

    Removed password from Email Server.

--- goResources/contactMe/contactMe.go
+++ goResources/contactMe/contactMe.go
@@ -1,12 +1,34 @@
 package contactMe
 
 import (
+	"encoding/json"
+	"fmt"
 	"gopkg.in/mail.v2"
+	"io/ioutil"
 	"strings"
 )
 
+type GoogleInfo struct {
+	Password            string
+}
+
+func getGoogleInfo() (GoogleInfo, error) {
+	file, err := ioutil.ReadFile("googleEmailPassword.json")
+	if err != nil {
+		fmt.Println("Error reading JSON file: ", err)
+	}
+	data := GoogleInfo{}
+	err = json.Unmarshal([]byte(file), &data)
+	return data, nil
+}
 
 func SendEmail(name, email, phone, message string) error {
+	googleInfo, err := getGoogleInfo()
+	if err != nil {
+		fmt.Println("Error obtaining our Google Email Info! ", err.Error())
+		return err
+	}
+
 	messageBody := []string{}
 	messageBody = append(messageBody, "Name: "+name)
 	messageBody = append(messageBody, "Email: "+email)
@@ -21,7 +43,7 @@ func SendEmail(name, email, phone, message string) error {
 	m.SetHeader("Subject", "Maxintosh Contact Request")
 	m.SetBody("text/plain", messageBodyStr)
 
-	d := mail.NewDialer("smtp.gmail.com", 587, "maxintosh.mailer", "17DesertHawk!")
+	d := mail.NewDialer("smtp.gmail.com", 587, "maxintosh.mailer", googleInfo.Password)
 	d.StartTLSPolicy = mail.MandatoryStartTLS
 
 	// Send the email to Bob, Cora and Dan.
