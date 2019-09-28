9d52e95bd4b20ac379ea235e521868b5e946eb93
brody prestwich <brody.prestwich18@gmail.com>
2019-09-28 13:49:33 -0600

    cleaned up the root directory

--- dbBackup/portfoliowebsite_db_public_ips.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.ips (id, ip, timestamp) VALUES (6, '::1', '2019-02-22 02:41:33.350487');
-INSERT INTO public.ips (id, ip, timestamp) VALUES (7, '172.16.3.43', '2019-02-22 03:32:02.887307');
-INSERT INTO public.ips (id, ip, timestamp) VALUES (8, '', '2019-02-27 00:46:46.205683');
