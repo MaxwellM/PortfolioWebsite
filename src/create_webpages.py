2bcd8468afe493dd96fb5c9831e54f9202e83c56
brody prestwich <brody.prestwich18@gmail.com>
2019-10-08 14:20:38 -0600

    added a watch feature to create_webpages.py

--- src/create_webpages.py
+++ src/create_webpages.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python3
-import os
+import os, sys, time
 
 pug_dir = "./pug/"
 less_dir = "./less/"
 public_dir = "../public/"
 css_dir = public_dir + "css/"
+dir_to_be_moved = ["js", "json", "images"]
 
-# copies directories from the src dir to the public dir with no clobber
-dir_to_be_moved = ["js", "json", "images", "unityGames", "node_modules"]
-for directory in dir_to_be_moved:
-    print("moving {} to public/{}".format(directory, directory))
-    os.system("cp -n -r {} ../public/".format(directory))
+def move_directories_to_public():
+    """copies directories from the src dir to the public dir"""
+    for directory in dir_to_be_moved:
+        os.system("cp -r {} ../public/".format(directory))
 
-# processes the html files from the pug files
-print("Converting pug files to HTML...")
-pug_files = os.listdir(pug_dir)
-print("Creating html files from {}".format(pug_files))
-for pug_file in pug_files:
-    if ".pug" in pug_file:
-        os.system("pug -P -o {} {}{} ".format(public_dir, pug_dir, pug_file))
+def convert_pug_to_html():
+    """processes the html files from the pug files"""
+    pug_files = os.listdir(pug_dir)
+    for pug_file in pug_files:
+        if ".pug" in pug_file:
+            os.system("pug -P -o {} {}{} ".format(public_dir, pug_dir, pug_file))
 
-# Converting less files to css
-print("Converting less files to CSS...")
-less_files = os.listdir(less_dir)
-print("Creating css files from {}".format(less_files))
-for less_file in less_files:
-    if ".less" in less_file:
-        if "_" not in less_file:
-            # removes the less from filename and adds css to the end
-            css_file = less_file[0:len(less_file)-4] + "css"
-            os.system("lessc {}{} {}{} ".format(less_dir, less_file, css_dir, css_file))
+def convert_less_to_css():
+    """Converting less files to css"""
+    less_files = os.listdir(less_dir)
+    for less_file in less_files:
+        if ".less" in less_file:
+            if "_" not in less_file:
+                # removes the less from filename and adds css to the end
+                css_file = less_file[0:len(less_file)-4] + "css"
+                os.system("lessc {}{} {}{} ".format(less_dir, less_file, css_dir, css_file))
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("moving {} to the public directory".format(dir_to_be_moved))
+        move_directories_to_public()
+        print("Converting pug files to HTML...")
+        convert_pug_to_html()
+        print("Converting less files to CSS...")
+        convert_less_to_css()
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "-w":
+            print("Watching...")
+            while True:
+                move_directories_to_public()
+                convert_pug_to_html()
+                convert_less_to_css()
+                time.sleep(5)
+    else:
+        print("add -w to watch")
