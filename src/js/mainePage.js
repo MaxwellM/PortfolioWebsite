7f8e9b4363fa1a7e793408b7353395fd3cfccbc3
MaxwellM <maxwellrmorin@gmail.com>
2020-02-25 16:24:49 -0700

    Added a github contribution chart!

--- src/js/mainePage.js
+++ src/js/mainePage.js
@@ -2,4 +2,6 @@ var ngModule = angular.module('app');
 
 ngModule.controller('mainPageCtrl', ['$scope', '$http', '$q', '$filter', function ($scope, $http, $q, $filter) {
 
+    GitHubCalendar(".calendar", "MaxwellM", { responsive: true });
+
 }]);
