0b71e714e75739e6ae3ae54e85874a6f9336f5f7
MaxwellM <maxwellrmorin@gmail.com>
2020-03-31 13:36:50 -0600

    Updated .gitignore file.

--- public/js/mainePage.js
+++
@@ -1,7 +0,0 @@
-var ngModule = angular.module('app');
-
-ngModule.controller('mainPageCtrl', ['$scope', '$http', '$q', '$filter', function ($scope, $http, $q, $filter) {
-
-    GitHubCalendar(".calendar", "MaxwellM", { responsive: true });
-
-}]);
