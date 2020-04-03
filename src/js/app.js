9f6d99becfec75f8db1ae27994c91c15a3acc609
MaxwellM <maxwellrmorin@gmail.com>
2020-04-02 18:41:22 -0600

    Default selected nav item is the main page.

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ ngModule.controller('myCtrl', ['$scope', '$http', '$q', '$filter', function ($sc
 
     $scope.playLostInSpace = false;
 
-    $scope.currentNavItem = '';
+    $scope.currentNavItem = 'Maxwell Morin';
     $scope.currentMenuItem = '';
 
     $scope.examples = [
