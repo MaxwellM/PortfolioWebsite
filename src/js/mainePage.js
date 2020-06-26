26acc715ce3994e945237d64fb745ffddce0d184
MaxwellM <maxwellrmorin@gmail.com>
2020-06-25 19:57:25 -0600

    Updated npm packages.

--- src/js/mainePage.js
+++ src/js/mainePage.js
@@ -2,11 +2,11 @@ var ngModule = angular.module('app');
 
 ngModule.controller('mainPageCtrl', ['$scope', '$rootScope', '$http', '$q', '$filter', function ($scope, $rootScope, $http, $q, $filter) {
 
-    GitHubCalendar(".calendar", "MaxwellM", {
+    GitHubCalendar(".calendar", "maxwellm", {
         responsive: true,
-        proxy: function (url) {
-            return "https://maxintosh.org/getGithubInfo/url?url="+ url;
-        }
-        });
-
+        tooltips: true,
+        proxy (username) {
+            return $http.get("https://maxintosh.org/getGithubInfo/url?url=https://github.com/users/"+username+"/contributions").then(r => r.data);
+        }}
+        );
 }]);
