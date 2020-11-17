b44a3d329ae0ccdc9918b4c5bc773f2036f1f933
MaxwellM <maxwellrmorin@gmail.com>
2020-11-17 12:24:33 -0700

    Improved progress bar for the Stock Tracker.

--- src/js/stockTracker.js
+++ src/js/stockTracker.js
@@ -24,36 +24,38 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
         }
     ];
 
-    // function getNewInventorySync() {
-    //     $scope.googleData =[];
-    //
-    //     console.log("START");
-    //     $q.all([
-    //         $http.get('/getNewInventory/url?url='+ 'https://www.bestbuy.com/site/nintendo-switch/nintendo-switch-consoles/pcmcat1484077694025.c?id=pcmcat1484077694025'),
-    //         $http.get('/getNewInventory/url?url='+ 'https://www.target.com/c/nintendo-switch-consoles-video-games/-/N-piakr'),
-    //         $http.get('/getNewInventory/url?url='+ 'https://www.walmart.com/search/?cat_id=2636_4646529_2002476&facet=retailer%3AWalmart.com'),
-    //         $http.get('/getNewInventory/url?url='+ 'https://www.gamestop.com/video-games/switch/consoles')
-    //     ]).then(function(results) {
-    //         console.log("END");
-    //         /* enter your logic here */
-    //         console.log(results);
-    //     });
-    // }
+    $scope.timer = function() {
+        let duration = Math.floor((Date.now() - $scope.start)/1000);
+        $scope.progressValue = 100 - ((duration / 300)* 100);
+        console.log("Percentage: ", $scope.progressValue);
+    }
 
     $scope.getDataPromise = function() {
+        //let started = Date.now();
+        $interval.cancel($scope.timerInterval);
         $scope.myLimit = 10;
         $scope.myPage = 1;
         $scope.progressValue = 0;
         $scope.results = [];
         // Lets loop through all of our vendors and gets some results!
+        const lastElement = $scope.vendors[$scope.vendors.length - 1];
+        let returnedPromises = 0;
         for(let vendor of $scope.vendors){
             let promise = $http.get('/getNewInventory', {params: {vendor: vendor.vendor, item: $scope.itemSelected}}).then(function (res) {
+                returnedPromises ++;
                 let results;
                 results = res.data;
                 if (results) {
                     $scope.results.push(...results);
                 }
                 $scope.progressValue += (100 / $scope.vendors.length);
+                if (returnedPromises === $scope.vendors.length) {
+                    //console.log("SECONDS: ", Math.floor((Date.now() - started) / 1000));
+                    //console.log("SECONDS: ", 300 - (Math.floor((Date.now() - started) / 1000)));
+                    //timer( 300 - (Date.now() - started));
+                    $scope.start = Date.now();
+                    $scope.timerInterval = $interval($scope.timer, 1000);
+                }
             }, function(error) {
                 alert(error.data);
             });
@@ -62,6 +64,7 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
         let today = new Date();
         // This will get AM/PM to show!
         $scope.lastUpdate= today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+
     }
 
     // starts the interval
@@ -77,6 +80,7 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
     // stops the interval
     $scope.stop = function() {
         $interval.cancel($scope.promise);
+        $interval.cancel($scope.timerInterval);
     };
 
     // If this gets destroyed (when leaving the tab) we'll stop the pinging!
