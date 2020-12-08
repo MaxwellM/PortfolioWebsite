70a41623e9ceb835abb33afc46f762bc47e7c6ef
MaxwellM <maxwellrmorin@gmail.com>
2020-12-08 14:51:23 -0700

    Think gmail is ready to test!

--- src/go/contactMe/contactMe.go
+++ src/go/contactMe/contactMe.go
@@ -1,23 +1,60 @@
 package contactMe
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"gopkg.in/mail.v2"
-	"strings"
-
-	"PortfolioWebsite/src/go/common"
-
-    "encoding/json"
-    "log"
-    "net/http"
-    "os"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-    "golang.org/x/net/context"
-    "golang.org/x/oauth2"
-    "golang.org/x/oauth2/google"
-    "google.golang.org/api/gmail/v1"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
 )
+
+var runningClient *http.Client
+var runningGMailService *gmail.Service
+
+func init() {
+    fmt.Println("INIT RUNNING!")
+    runningClient, runningGMailService = getMyStuff()
+    //setupCredentials()
+}
+
+func getMyStuff() (*http.Client, *gmail.Service) {
+	// Reads in our credentials
+	secret, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Printf("Error Reading Credentials: %v", err)
+	}
+
+	// Creates a oauth2.Config using the secret
+	// The second parameter is the scope, in this case we only want to send email
+	conf, err := google.ConfigFromJSON(secret, gmail.GmailSendScope)
+	if err != nil {
+		log.Printf("Error Creating CONF: %v", err)
+	}
+
+	// Exchange the auth code for an access token
+	tok, err := tokenFromFile("token.json")
+	if err != nil {
+		log.Printf("Error Reading Token: %v", err)
+	}
+
+	// Create the *http.Client using the access token
+	runningClient = conf.Client(oauth2.NoContext, tok)
+
+	// Create a new gmail service using the client
+	gmailService, err := gmail.New(runningClient)
+	if err != nil {
+		log.Printf("Error Creating GMail Service: %v", err)
+
+	}
+	return runningClient, gmailService
+}
 
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
@@ -74,32 +111,32 @@ func saveToken(path string, token *oauth2.Token) {
         json.NewEncoder(f).Encode(token)
 }
 
-func SendEmail(name, email, phone, message string) error {
+func setupCredentials() {
         b, err := ioutil.ReadFile("credentials.json")
         if err != nil {
                 log.Fatalf("Unable to read client secret file: %v", err)
-                return err
+
         }
 
         // If modifying these scopes, delete your previously saved token.json.
-        config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+        config, err := google.ConfigFromJSON(b, gmail.GmailSendScope)
         if err != nil {
                 log.Fatalf("Unable to parse client secret file to config: %v", err)
-                return err
+
         }
         client := getClient(config)
 
         srv, err := gmail.New(client)
         if err != nil {
                 log.Fatalf("Unable to retrieve Gmail client: %v", err)
-                return err
+
         }
 
         user := "me"
         r, err := srv.Users.Labels.List(user).Do()
         if err != nil {
                 log.Fatalf("Unable to retrieve labels: %v", err)
-                return err
+
         }
         if len(r.Labels) == 0 {
                 fmt.Println("No labels found.")
@@ -108,39 +145,32 @@ func SendEmail(name, email, phone, message string) error {
         for _, l := range r.Labels {
                 fmt.Printf("- %s\n", l.Name)
         }
-        return nil
 }
 
-func SendEmail2(name, email, phone, message string) error {
-    googleEmailInfo, err := common.ReadJsonFile("googleEmailPassword.json")
-    if err != nil {
-        fmt.Println("Error reading JSON file!")
-        return err
-    }
-    googleEmailPassword := googleEmailInfo["Password"].(string)
+func SendEmail(name, email, phone, message string) error {
+ 	// New message for our gmail service to send
+ 	var fullMessage gmail.Message
 
-	messageBody := []string{}
-	messageBody = append(messageBody, "Name: "+name)
-	messageBody = append(messageBody, "Email: "+email)
-	messageBody = append(messageBody, "Phone: "+phone)
-	messageBody = append(messageBody, "Message: "+message)
-	messageBodyStr := strings.Join(messageBody, "\n")
+ 	// Compose the message
+ 	messageStr := []byte(
+ 		"From: maxintosh.mailer@gmail.com\r\n" +
+        "To: "+ email + "\r\n" +
+        "Subject: Maxintosh Contact Request\r\n\r\n" +
+		"Name: " + name + "\n" +
+		"Email: " + email + "\n" +
+		"Phone: " + phone + "\n" +
+		"Message: " + message)
 
-	m := mail.NewMessage()
-	m.SetHeader("From", "maxintosh.mailer@gmail.com")
-	m.SetHeader("To", "maxwellrmorin@gmail.com")
-	m.SetAddressHeader("Cc", email, "Mailer")
-	m.SetHeader("Subject", "Maxintosh Contact Request")
-	m.SetBody("text/plain", messageBodyStr)
+ 	// Place messageStr into message.Raw in base64 encoded format
+	fullMessage.Raw = base64.URLEncoding.EncodeToString(messageStr)
 
-	d := mail.NewDialer("smtp.gmail.com", 587, "maxintosh.mailer", googleEmailPassword)
-	d.StartTLSPolicy = mail.MandatoryStartTLS
-
-	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
-	    fmt.Println("Error: ", err.Error())
-		return err
-	} else {
-		return nil
-	}
+ 	// Send the message
+ 	_, err := runningGMailService.Users.Messages.Send("me", &fullMessage).Do()
+ 	if err != nil {
+ 		log.Printf("Error: %v", err)
+ 		return err
+ 	} else {
+ 		fmt.Println("Message sent!")
+ 		return nil
+ 	}
 }
