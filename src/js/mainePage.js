23dce9c3757d87376d9103cfb2f6b914ad2d468d
MaxwellM <maxwellrmorin@gmail.com>
2021-01-19 17:14:30 -0700

    Updated GitHubCalendar

--- src/js/mainePage.js
+++ src/js/mainePage.js
@@ -1,12 +1,20 @@
 var ngModule = angular.module('app');
 
 ngModule.controller('mainPageCtrl', ['$scope', '$rootScope', '$http', '$q', '$filter', function ($scope, $rootScope, $http, $q, $filter) {
+    $scope.totalContributions = 0;
 
     GitHubCalendar(".calendar", "maxwellm", {
         responsive: true,
         tooltips: true,
-        proxy (username) {
-            return $http.get("https://maxintosh.org/getGithubInfo/url?url=https://github.com/users/"+username+"/contributions").then(r => r.data);
-        }}
+        global_stats: true,
+            proxy: function (username) {
+                return $http.get("https://maxintosh.org/getGithubInfo/url?url=https://github.com/users/" + username + "/contributions").then(r => r.data);
+            }}
         );
+
+    // This waits for GitHubCalendar to be done loading...
+    angular.element(function () {
+        $scope.totalContributions = document.getElementById("contribNum").innerText.split(" ", 1)[0];
+        $scope.$apply();
+    });
 }]);
