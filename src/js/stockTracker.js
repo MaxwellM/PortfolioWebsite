86c4e6ff7aec5ebfbd2e19a1c81132bfec51ec79
MaxwellM <maxwellrmorin@gmail.com>
2021-01-22 11:41:33 -0700

    Updated Stock Tracker

--- src/js/stockTracker.js
+++ src/js/stockTracker.js
@@ -2,12 +2,13 @@ var ngModule = angular.module('app');
 
 ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$sanitize', '$interval', function ($scope, $http, $q, $filter, $sanitize, $interval) {
     //$scope.getNewInventory = getNewInventory;
+    let fiveMinTime = null;
     $scope.progressValue = 0;
     $scope.lastUpdate = null;
     $scope.promise = null;
     $scope.getDataPromise = null;
     $scope.results = [];
-    $scope.itemSelected = "Nintendo Switch";
+    $scope.itemSelected = "XBox Series X";
     $scope.items = ['Nintendo Switch', 'Dyson V11 Vacuum', 'Apple AirPods', 'XBox Series X', 'XBox Series S'];
     $scope.vendors = [
         {   url: "",
@@ -24,23 +25,42 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
         }
     ];
 
-    $scope.timer = function() {
-        let duration = Math.floor((Date.now() - $scope.start)/1000);
-        $scope.progressValue = 100 - ((duration / 300)* 100);
-        console.log("Percentage: ", $scope.progressValue);
+    // Got this here: https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+    function millisToMinutesAndSeconds(millis) {
+        const minutes = Math.floor(millis / 60000);
+        const seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
     }
 
-    $scope.getDataPromise = function() {
-        //let started = Date.now();
+    $scope.timer = function() {
+        let now = Date.now();
+        $scope.duration = Math.floor((now - $scope.start)/1000);
+        $scope.timeLeft = millisToMinutesAndSeconds(fiveMinTime - now);
+        $scope.progressValue = 100 - (($scope.duration / 300)* 100);
+        //console.log("Percentage: ", $scope.progressValue);
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    $scope.getDataPromise = async function() {
         $interval.cancel($scope.timerInterval);
         $scope.myLimit = 10;
         $scope.myPage = 1;
         $scope.progressValue = 0;
+        $scope.progressValueBuffer = 0;
+        $scope.currentVendor = "";
+        $scope.timeLeft = null;
         $scope.results = [];
         // Lets loop through all of our vendors and gets some results!
         const lastElement = $scope.vendors[$scope.vendors.length - 1];
         let returnedPromises = 0;
         for(let vendor of $scope.vendors){
+            $scope.progressValueBuffer += (100 / $scope.vendors.length);
+            $scope.currentVendor = vendor.vendor;
+            // We can add a sleep, if we want.
+            // await sleep(1000);
             let promise = $http.get('/getNewInventory', {params: {vendor: vendor.vendor, item: $scope.itemSelected}}).then(function (res) {
                 returnedPromises ++;
                 let results;
@@ -49,22 +69,23 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
                     $scope.results.push(...results);
                 }
                 $scope.progressValue += (100 / $scope.vendors.length);
+                // On the last pass, do this...
                 if (returnedPromises === $scope.vendors.length) {
-                    //console.log("SECONDS: ", Math.floor((Date.now() - started) / 1000));
-                    //console.log("SECONDS: ", 300 - (Math.floor((Date.now() - started) / 1000)));
-                    //timer( 300 - (Date.now() - started));
                     $scope.start = Date.now();
+                    // Five min into the future!
+                    fiveMinTime = ($scope.start + 300000);
                     $scope.timerInterval = $interval($scope.timer, 1000);
+                    // Lets update lastUpdate with our new time!
+                    let today = new Date();
+                    $scope.currentVendor = "";
+                    $scope.timeLeft = millisToMinutesAndSeconds(fiveMinTime - $scope.start);
+                    // This will get AM/PM to show!
+                    $scope.lastUpdate = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
                 }
             }, function(error) {
                 alert(error.data);
             });
         }
-        // Lets update lastUpdate with our new time!
-        let today = new Date();
-        // This will get AM/PM to show!
-        $scope.lastUpdate= today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
-
     }
 
     // starts the interval
@@ -75,6 +96,7 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
         // store the interval promise
         // Also run it once every 5 minutes. Don't want to go too crazy.
         $scope.promise = $interval($scope.getDataPromise, 300000);
+
     };
 
     // stops the interval
@@ -89,10 +111,6 @@ ngModule.controller('stockTrackerCtrl', ['$scope', '$http', '$q', '$filter', '$s
         $interval.cancel($scope.promise);
         $interval.cancel($scope.timerInterval);
     });
-
-    function quickWindowResize() {
-
-    }
 
     $scope.getDataPromise();
     $scope.start();
